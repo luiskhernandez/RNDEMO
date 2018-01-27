e25fbd0cd47f2c5f1f290ff0147e309b631e9904
Luis Hernandez <luisk.hernandez.macias@gmail.com>
2018-01-27 09:22:42 -0500

    Fix keyboar fixes

--- App.js
+++ App.js
@@ -5,7 +5,7 @@
  */
 
 import React, { Component } from 'react';
-import ReactNative, { Animated, Keyboard, StyleSheet, Text, View, TextInput, ScrollView, Platform } from 'react-native';
+import ReactNative, { Container, KeyboardAvoidingView, Animated, Keyboard, StyleSheet, Text, View, TextInput, ScrollView, Platform } from 'react-native';
 import {KeyboardAwareScrollView} from "react-native-keyboard-aware-scroll-view";
 
 import { Col, Row, Grid } from "react-native-easy-grid";
@@ -17,173 +17,64 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-// width Grid
-// export default class App extends Component<{}> {
-//   render() {
-//     return (
-//       <Grid>
-//           <Row size={3} style={styles.topSection}>
-//             <Text style={styles.welcome}>Hola mundo 23</Text>
-//           </Row>
-//           <Row size={1}>
-//             <View style={styles.container}>
-//               <TextInput placeholder="Simple test" />
-//               <TextInput placeholder="another input" />
-//             </View>
-//             <View style={{ height: 60 }} />
-//           </Row>
-//       </Grid>
-//     );
-//   }
-// }
+function KeyboardWrapper({ children }) {
+  return Platform.OS === 'ios'
+    ? <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }} keyboardVerticalOffset={60}>
+        {children}
+      </KeyboardAvoidingView>
+    : <View style={{ flex: 1 }}>
+        {children}
+      </View>;
+}
 
 // with KeyboardAvoidingView
-//export default class App extends Component<{}> {
-  // render() {
-  //   return (
-  //   <KeyboardAvoidingView style={{flex: 1, height: '100%' }}>
-  //     <Grid>
-  //         <Row size={3} style={styles.topSection}>
-  //           <Text style={styles.welcome}>Hola mundo 23</Text>
-  //         </Row>
-  //         <Row size={1}>
-  //           <View style={styles.container}>
-  //             <TextInput placeholder="Simple test" />
-  //             <TextInput placeholder="another input" />
-  //           </View>
-  //           <View style={{ height: 60 }} />
-  //         </Row>
-  //     </Grid>
-  //   </KeyboardAvoidingView>
-  //   );
-  // }
-//}
-
-export default class App extends Component {
-
-  state = {
-    keyboardHeight: new Animated.Value(0)
-  };
-
-  // animateKeyboardHeight = (toValue, duration) => {
-  //   Animated.timing(
-  //     this.state.keyboardHeight,
-  //     {toValue, duration},
-  //   ).start();
-  // };
-
-  /**
-   * From https://facebook.github.io/react-native/docs/keyboard.html#addlistener
-   * "Note that if you set android:windowSoftInputMode to adjustResize or adjustNothing,
-   * only keyboardDidShow and keyboardDidHide events will available on Android."
-   */
-  componentWillMount() {
-    // if (Platform.OS === "android") {
-    //   this.keyboardShowListener = Keyboard.addListener("keyboardDidShow", ({endCoordinates}) => {
-    //     this.animateKeyboardHeight(endCoordinates.height, 0)
-    //   });
-    //   this.keyboardHideListener = Keyboard.addListener("keyboardDidHide", () => {
-    //     this.animateKeyboardHeight(0, 300)
-    //   })
-    // }
-  }
-
-  scrollToInput = (reactNode) => {
-    this.view.scrollToFocusedInput(reactNode)
-  };
-
-  handleOnFocus = (e) => {
-    if (Platform.OS === "android") {
-      this.scrollToInput(ReactNative.findNodeHandle(e.target))
-    }
-  };
-
-  render() {
+export default class App extends Component<{}> {
+   render() {
     const spacer = (
       <View style={styles.spacer}>
         <Text style={styles.text} numberOfLines={24}>
-          {"Baila como juana la cubana. ".repeat(30)}
+          {"Baila como juana la cubana. ".repeat(300)}
         </Text>
       </View>
     );
-    return (
-      <KeyboardAwareScrollView
-        ref={ref => this.view = ref}
-        style={styles.container}
-        enableOnAndroid
-        extraHeight={Platform.OS === "android" ? 10 : undefined}
-      >
-        {spacer}
-        <TextInput
-          onFocus={this.handleOnFocus}
-          style={styles.input}
-        />
-        {/*{spacer}*/}
-
-      </KeyboardAwareScrollView>
-    );
-  }
+     return (
+      <KeyboardWrapper>
+         <Grid style={{backgroundColor: 'yellow' }}>
+             <Row size={90} style={styles.topSection}>
+                 {spacer}
+             </Row>
+             <Row size={10}>
+               <View style={styles.container}>
+                 <TextInput placeholder="Simple test" />
+                 <TextInput placeholder="another input" />
+               </View>
+               <View style={{ height: 60 }} />
+             </Row>
+         </Grid>
+         </KeyboardWrapper>
+     );
+   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 10,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
+    backgroundColor: '#F5FCFF',
   },
-  spacer: {
-    minHeight: 500,
-    justifyContent: "center",
-    alignItems: "center",
+  topSection: {
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  text: {
-    fontFamily: Platform.OS === "android" ? "monospace" : "Courier",
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
   },
-  input: {
-    borderColor: "grey",
-    borderRadius: 6,
-    borderWidth: 1,
-    padding: 10,
-  }
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
 });
-
-// export default class App extends Component<{}> {
-//   render() {
-//     return (
-//       <View style={styles.containerr}>
-//         <TextInput placeholder="Simple test" />
-//         <TextInput placeholder="another input" />
-//         <View style={{ height: 60 }} />
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   containerr: {
-//     flex: 1,
-//     flexDirection: 'column',
-//     justifyContent: 'flex-end',
-//     height: '100%'
-//   },
-//   container: {
-//     flex: 1,
-//     flexDirection: 'column',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   topSection: {
-//     backgroundColor: 'red',
-//     justifyContent: 'center',
-//     alignItems: 'center'
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
